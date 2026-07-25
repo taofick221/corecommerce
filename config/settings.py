bff69f0a6da4694e0cc3097bb57f0e1b9a89afd8
Taofick <mdtaofick45418@gmail.com>
2026-07-25 11:14:05 +0600

    Fix DEFAULT_AUTO_FIELD

--- config/settings.py
+++ config/settings.py
@@ -151,6 +151,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
